c8d4e81b7a08c42fee6bd56515de8e4b456b2d39
sanjay.kesari28 <sanjay.kesari28@gmail.com>
2019-07-17 13:51:59 +0530

    Update study table structure and rewlated queries

--- salestudy.sql
+++ salestudy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 15, 2019 at 07:18 PM
+-- Generation Time: Jul 17, 2019 at 01:50 PM
 -- Server version: 5.7.26-0ubuntu0.18.04.1
 -- PHP Version: 7.2.19-0ubuntu0.18.04.1
 
@@ -39,8 +39,10 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `name`, `city`, `industry`, `type`) VALUES
-(1, 'X', 'delhi', 'software', 1),
-(2, 'Y', 'noida', 'health', 2);
+(1, 'Samsonic', 'pleasant', 'J', 1),
+(2, 'Panasung', 'oaktown', 'J', 2),
+(3, 'Samony', 'jackson', 'B', 1),
+(4, 'Orange', 'Jackson', 'B', 2);
 
 -- --------------------------------------------------------
 
@@ -61,10 +63,13 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `order_date`, `cust_id`, `salesperson_id`, `amount`) VALUES
-(1, '2019-07-11 00:00:00', 1, 2, 500),
-(2, '2019-07-13 00:00:00', 2, 1, 700),
-(3, '2018-12-14 00:00:00', 1, 1, 55),
-(4, '2018-05-09 00:00:00', 2, 3, 333);
+(1, '2012-07-14 00:00:00', 4, 1, 460),
+(2, '2012-01-29 00:00:00', 3, 2, 2400),
+(3, '2012-01-30 00:00:00', 1, 5, 1800),
+(4, '2012-08-02 00:00:00', 1, 2, 540),
+(5, '2012-02-03 00:00:00', 2, 4, 600),
+(6, '2012-03-02 00:00:00', 2, 4, 720),
+(7, '2012-05-06 00:00:00', 4, 4, 150);
 
 -- --------------------------------------------------------
 
@@ -84,10 +89,12 @@ CREATE TABLE `salesperson` (
 --
 
 INSERT INTO `salesperson` (`id`, `name`, `age`, `salary`) VALUES
-(1, 'A', 30, 1000),
-(2, 'B', 35, 2000),
-(3, 'C', 55, 9999),
-(4, 'D', 88, 56666);
+(1, 'Abe', 61, 140000),
+(2, 'Bob', 34, 44000),
+(3, 'Chris', 34, 40000),
+(4, 'Dan', 41, 52000),
+(5, 'Ken', 57, 115000),
+(6, 'Joe', 38, 38000);
 
 --
 -- Indexes for dumped tables
@@ -120,12 +127,12 @@ ALTER TABLE `salesperson`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `salesperson`
 --
 ALTER TABLE `salesperson`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- Constraints for dumped tables
 --
